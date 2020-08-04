e974c5d288b08d05506c3cb594cda17fc581fa19
Marja Satukangas <marja.satukangas@metropolia.fi>
2020-08-04 23:53:57 +0300

    listing customers and trainings done

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,17 +1,49 @@
 import React, {useState, useEffect} from 'react'
-import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import Trainings from './Trainings'
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
-import Collapse from '@material-ui/core/Collapse';
+import { forwardRef } from 'react';
+import 'react-table/react-table.css'
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
 
 export default function Customers() {
+
+   const tableIcons = {
+      Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+      Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+      Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+      Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+      DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+      Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+      Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+      Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+      FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+      LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+      NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+      PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+      ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+      Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+      SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+      ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+      ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    }
     
     const [customers, setCustomers] = useState([])
-    const [open, setOpen] = useState(false);
     
     useEffect(() => fetchData(), [])
     
@@ -24,44 +56,41 @@ export default function Customers() {
    const columns = [
       
       {
-         Header: 'Firstname',
-         accessor: 'firstname'
+         title: 'Firstname',
+         field: 'firstname'
       },
       {
-         Header: 'Lastname',
-         accessor: 'lastname' 
+         title: 'Lastname',
+         field: 'lastname' 
       },
       {
-         Header: 'Streetaddress',
-         accessor: 'streetaddress' 
+         title: 'Streetaddress',
+         field: 'streetaddress' 
       },
       {
-         Header: 'Postcode',
-         accessor: 'postcode' 
+         title: 'Postcode',
+         field: 'postcode' 
       },
       {
-         Header: 'City',
-            accessor: 'city' 
+         title: 'City',
+         field: 'city' 
       },
       {
-         Header: 'Email',
-         accessor: 'email' 
+         title: 'Email',
+         field: 'email' 
       },
       {
-         Header: 'Phone',
-         accessor: 'phone' 
+         title: 'Phone',
+         field: 'phone' 
       },
       {
-         sortable: false,
-         filterable: false,
-         width: 100,
-         Cell: row => <Trainings customer={row.original} /> 
+         render: rowData => <Trainings customer={rowData} /> 
       },        
     ]
 
     return ( 
         <div>
-            <ReactTable filterable={true} data={customers} columns={columns}/>
+            <MaterialTable  title='Customers' options={{pageSize: 20}} icons={tableIcons} filterable={true} data={customers} columns={columns} />
         </div>
     )
 }
--- src/components/Trainings.js
+++ src/components/Trainings.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import ReactTable from 'react-table'
+import React, {useState} from 'react'
+import { forwardRef } from 'react';
 import 'react-table/react-table.css'
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,15 +7,53 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import moment from 'moment';
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
 
 
 export default function Trainings(props) {
+
+    const tableIcons = {
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    }
 
     const [open, setOpen] = useState(false)
     const [trainings, setTrainings] = useState([])
 
 
     const handleClickOpen = () => {
+        console.log(props.customer)
         setOpen(true)
         fetch(props.customer.links[2].href)
         .then(response => response.json())
@@ -29,17 +67,17 @@ export default function Trainings(props) {
 
     const columns = [
         {
-           Header: 'Date',
-           accessor: 'date',
-           Cell: row => moment(row.value).format('MMMM Do YYYY') 
+           title: 'Date',
+           field: 'date',
+           render: row => moment(row.value).format('MMMM Do YYYY') 
         },
         {
-           Header: 'Duration (min)',
-           accessor: 'duration' 
+           title: 'Duration (min)',
+           field: 'duration' 
         },
         {
-            Header: 'Activity',
-            accessor: 'activity' 
+            title: 'Activity',
+            field: 'activity' 
          },
     ]
 
@@ -49,11 +87,11 @@ export default function Trainings(props) {
         <Button color="primary" onClick={handleClickOpen}>
           Trainings
         </Button>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">{'Trainings - ' + props.customer.firstname + " " + props.customer.lastname}</DialogTitle>
-                <DialogContent>
-                    <ReactTable filterable={true} pageSize={10} data={trainings} columns={columns}/>
-                </DialogContent>
+        <Dialog fullWidth={true} open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle>Trainings</DialogTitle>
+            <DialogContent>
+                <MaterialTable emptyRowsWhenPaging={false} icons={tableIcons} title={props.customer.firstname + " " + props.customer.lastname} filterable={true} data={trainings} columns={columns}/>
+            </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="primary">Close</Button>
             </DialogActions>
